f3db08b7fcd470511c791f5554b0e93feb5f906c
romanov.v <romanov.v@wnfx.ru>
2025-02-24 22:40:16 +0300

    add files

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../config/config.php';
 
 use App\Infrastructure\Database\DatabaseConnection;
 use App\Infrastructure\Database\TransactionRepository;
